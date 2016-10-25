416cc85fcbf31cf6327b55d01b07d7abd643760e
topseer <jackyangxu@gmail.com>
2016-10-24 22:46:32 -0400

    Update panda.py

--- panda.py
+++ panda.py
@@ -3,3 +3,6 @@ iris_data.unique()
 
 #%%updateData
 iris_data.loc[iris_data['class'] == 'versicolor', 'class'] = 'Iris-versicolor'
+
+#so that pandas knows to treat rows with 'NA' as missing values.
+iris_data = pd.read_csv('iris.csv', na_values=['NA'])
