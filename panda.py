82a4858fd8ecc4d8317aaa70bd0a62352d3bd2ea
topseer <jackyangxu@gmail.com>
2016-10-25 11:36:57 -0400

    Update panda.py

--- panda.py
+++ panda.py
@@ -4,6 +4,9 @@ iris_data.unique()
 #%%updateData
 iris_data.loc[iris_data['class'] == 'versicolor', 'class'] = 'Iris-versicolor'
 
+#%%where condition
+borrEmail.loc[borrEmail['Borremail'] =="mmnfreemason05@verizon.net"]
+
 #so that pandas knows to treat rows with 'NA' as missing values.
 iris_data = pd.read_csv('iris.csv', na_values=['NA'])
 
