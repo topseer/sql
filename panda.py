a0dbb559bc0a965daf313024e921027cdfc475c4
topseer <jackyangxu@gmail.com>
2016-10-24 22:20:08 -0400

    Update panda.py

--- panda.py
+++ panda.py
@@ -1,3 +1,12 @@
 # return unique values
 Iris_data.unique()
-###
+
+#update Data
+iris_data.loc[iris_data['class'] == 'versicolor', 'class'] = 'Iris-versicolor'
+
+#%%test
+this is a test
+
+
+#%%test
+this is another test
