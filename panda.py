4ae9c20b5e527edad7cb0cc107a497b775b4342f
topseer <jackyangxu@gmail.com>
2016-10-24 23:13:51 -0400

    Update panda.py

--- panda.py
+++ panda.py
@@ -6,3 +6,10 @@ iris_data.loc[iris_data['class'] == 'versicolor', 'class'] = 'Iris-versicolor'
 
 #so that pandas knows to treat rows with 'NA' as missing values.
 iris_data = pd.read_csv('iris.csv', na_values=['NA'])
+
+#summary 
+iris_data.describe()
+
+#drop rows by condition
+iris_data = iris_data.loc[(iris_data['class'] != 'Iris-setosa') | (iris_data['sepal_width_cm'] >= 2.5)]
+iris_data.loc[iris_data['class'] == 'Iris-setosa', 'sepal_width_cm'].hist()
