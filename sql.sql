c822df15d5da62993fda5412c4fc345e6ee658a5
topseer <jackyangxu@gmail.com>
2016-11-30 11:48:36 -0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,1 +1,7 @@
-
+      WITH cte
+         AS (SELECT ROW_NUMBER() OVER (PARTITION BY startDate, endDate, adwordsCampaignID,adwordsAdGroupID,adwordsCreativeID,adCost
+                                           ORDER BY ( SELECT 0)) RN
+             FROM   test_yang.dbo.googleADWordsData
+			 )
+      DELETE FROM cte
+      WHERE  RN > 1;
